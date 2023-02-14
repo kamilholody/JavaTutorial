22f8b6d195a34fd737edd78ebc6c3279d0efd3ba
Kamil Holody <kamilholody@live.com>
2023-02-14 17:16:47 +0100

    sample commit mesagge

---
+++ src/TypyProste.java
@@ -0,0 +1,28 @@
+public class TypyProste {
+
+    public static void main(String[] args) {
+        // Liczby stałoprzecinkowe
+        byte firstNumber = 127; // 1 bajt -128 do 127
+        short secondNumber = 32689; // 2 bajty - 32768 do 32767
+        int thirdNumber = 32768999; // 4 bajty -2 147 483 648 do 2 147 483 647
+        long fourthNumber = 2148000L; // 8 bajtów -2^63 do (2^63)-1
+
+        //liczby zmienneprzecinkowe
+        float fifthNumber = 4.99934F;
+        double sixtNumber = 3.999999999999999D;
+
+        // wartość logiczna
+        boolean prawda = true;
+        boolean fałsz = false;
+
+        // pojedynczy znak
+
+        char letter = 'A';
+
+        // ciągi znaków
+
+        String hello = "Hello Kamil";
+        
+    }
+
+}
